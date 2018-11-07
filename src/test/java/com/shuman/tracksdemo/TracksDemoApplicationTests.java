58f24815887393f61610b6f61fbc10a94a4eb908
shuman_rev <anton.shumskih@revolut.com>
2018-11-07 19:21:13 +0300

    tests infra

--- src/test/java/com/shuman/tracksdemo/TracksDemoApplicationTests.java
+++ src/test/java/com/shuman/tracksdemo/TracksDemoApplicationTests.java
@@ -5,8 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TracksDemoApplication.class)
@@ -17,16 +27,22 @@ public class TracksDemoApplicationTests {
     private MockMvc mvc;
 
     @Test
-    public void test() throws Exception {
-            /*mvc.perform(get("/api/employees")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                    .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].name").value("bob"));*/
+    public void create_track() throws Exception {
+        mvc.perform(post("/tracks")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(readFile("create_track.json")))
+            .andExpect(status().isOk())
+            .andExpect(content()
+                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].name").value("bob"));
     }
 
     @Test
     public void contextLoads() {
+    }
+
+    private String readFile(String fileName) throws IOException {
+        String path = getClass().getClassLoader().getResource(fileName).getPath();
+        return Files.readString(Paths.get(path));
     }
 }
